7f361dcf8d0bf64750ee794bbd562ca1bbfff104
Michael Geitz <mrgeitz@gmail.com>
2017-03-10 17:51:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+>       ________     ___   __   _______  ______
+>      / __/ __ \   / _ | / /  / __/ _ \/_  __/
+>     / _// /_/ /  / __ |/ /__/ _// , _/ / /
+>    /___/\___\_\ /_/ |_/____/___/_/|_| /_/
+>   ######PROJECT 1999 EQ EMU PARSER######
+---
+
+## Dependencies
+    - espeak
+    - alsa
+    - python 2.7
+
+## Getting Started
+
+  - Populate settings/paths, settings/sounds, characters/default
+  - Add sounds added in settings/sounds to sounds directory
+
+
+## Controls
+
+  - q  : Quit/Back
+  - h  : Toggle heal parse
+  - s  : Toggle spell damage parse
+  - p  : Toggle all parses
+  - c  : Clear event box
+  - r  : Toggle raid mode
+  - F1 : Display help menu
+  - F2 : Open char menu
+  - F3 : Save and reset parses
+  - F4 : Clear heal parse history
+  - F5 : Clear spell parse history
+  - F12: Reload config file
+
+
+## Modes
+
+  - Raid Mode
+    > espeaks all raid values in alert/guild in config
+    > toggle with 'r'
+    > auto-enables when zoning into 'raid' zone
+
+  - Heal Parse
+    > tallies all heals on targets
+    > 'h' to toggle heal parse
+    > F3 to save heal parse to file
+    > F4 to clear the heal parse
+
+  - Spell Parse
+    > tallies spell damage done to a target
+    > 's' to toggle spell parse
+    > F3 to save spell parse to file
+    > F5 to clear spell parse
+
+
+## Adding new sounds, triggers
+
+  - Add new sounds to the sound directory, their paths manually in settings/sounds
+  - Add new triggers to alert in the config file
+
+## Adding new characters
+
+  - All log files seen at start up in log root path are automatically added to the config as a character
+
+
+## Adding new line_type to be parsed
+
+  - Add to if/elif structure in eqparser.py, minding existing control flow
+  - Default values are generated in config in settings/sound_settings, settings/check_line_type, and alert
+    for added line_types once encountered in-game
---
+++ eqaparser.py
@@ -0,0 +1,221 @@
+#! /usr/bin/env python
+
+"""
+Returns the determined line type of a line in eq.log
+"""
+__author__ = "Michael Geitz"
+__version__ = "0.1.1"
+
+def read(log_path):
+    """Reads and returns the eqlog.txt file"""
+
+    with open(log_path) as f:
+        content = f.readlines()
+    return content
+
+
+def determine(line, line_list):
+    """Determine type of line"""
+
+    line_type = "undetermined"
+
+
+   # chat your player initiates
+    if line_list[0] == "you":
+        if line_list[1] == "told":
+            line_type = "you_tell"
+        elif line_list[1] == "say,":
+            line_type = "you_say"
+        elif line_list[1] == "shout,":
+            line_type = "you_shout"
+        elif "party," in line:
+            line_type = "you_group"
+        elif line_list[1] == "say":
+            if line_list[4] == "character,":
+                line_type = "you_ooc"
+            if line_list[4] == "guild,":
+                line_type = "you_guild"
+        elif line_list[1] == "auction,":
+            line_type = "you_auction"
+            if "wts" in line or "selling" in line and "wtb" in line or "buying" in line:
+                line_type = "you_auction_wts:wtb"
+            if "wts" in line or "selling" in line:
+                line_type = "you_auction_wts"
+            if "wtb" in line or "buying" in line:
+                line_type = "you_auction_wtb"
+        elif line_list[1] == "have":
+            if line_list[2] == "entered":
+                line_type = "you_new_zone"
+            elif line_list[2] == "healed":
+                line_type = "you_healed"
+        elif "hungry." in line_list:
+            line_type = "you_hungry"
+        elif line_list[1] == "are":
+            if line_list[2] == "now":
+                if "a.f.k." in line:
+                    line_type = "you_afk_on"
+                elif line.endswith("looking for a group."):
+                    line_type = "you_lfg_on"
+            elif line_list[2] == "no":
+                if "a.f.k." in line:
+                    line_type = "you_afk_off"
+                elif line.endswith("looking for a group."):
+                    line_type = "you_lfg_off"
+            elif line_list[2] == "out":
+                if line_list[-1] == "food." and "drink" not in line and "drink." not in line:
+                    line_type = "you_outfood"
+                elif line_list[-1] == "drink." and "food" not in line and "food." not in line:
+                    line_type = "you_outdrink"
+                elif line.endswith("food and drink."):
+                    line_type = "you_outfooddrink"
+                elif line.endswith("drink and low on food."):
+                    line_type = "you_outdrinklowfood"
+                elif line.endswith("food and low on drink."):
+                    line_type = "you_outfoodlowdrink"
+            elif line_list[-1] == "thirsty.":
+                line_type = "you_thirsty"
+            elif line_list[-1] == "hungry.":
+                line_type = "you_hungry"
+        elif line_list[1] == "forget":
+            line_type == "you_spell_forget"
+
+    elif line_list[0] == "your":
+        if line_list[2] == "spell":
+            if line_list[1] == "charm":
+                line_type = "spell_break_charm"
+            elif line_list[1] == "ensnare":
+                line_type = "spell_break_ensare"
+            else:
+                line_type = "spell_break"
+        elif line_list[1] ==  "faction":
+            line_type = "faction_line"
+        elif line_list[1] == "target":
+            if line_list[4] == "cured.":
+                line_type = "target_cured"
+    elif line_list[0].startswith("-"):
+        line_type = "who_line"
+    elif len(line_list) == 1:
+        line_type = "mysterious_oner"
+
+    # chat from other players
+    elif line_list[1] == "tells":
+        if line_list[2] == "you,":
+            line_type = "tell" # or emote
+        if line_list[3] == "guild,":
+            line_type = "guild" # or emote
+        if line_list[3] == "group,":
+            line_type = "group" # or emote
+    elif line_list[1] == "says,":
+        line_type = "say" # or emote
+    elif line_list[1] == "shouts,":
+        line_type = "shout" # or emote
+    elif line_list[1] == "says":
+        if line_list[4] == "character,":
+            line_type = "ooc" # or emote
+    elif line_list[1] == "auctions,":
+        line_type = "auction" # or emote
+        #if "wts" in line or "selling" in line and "wtb" in line or "buying" in line:
+        #    line_type = "auction_wts:wtb"
+        if "wts" in line or "selling" in line:
+            line_type = "auction_wts"
+        elif "wtb" in line or "buying" in line:
+            line_type = "auction_wtb"
+
+    # spells / casting
+    elif line_list[1] == "spell":
+        line_type = "spell_something"
+        if line_list[2] == "fizzles!":
+            line_type = "spell_fizzle"
+    elif line_list[1] == "casting":
+        if line_list[3] == "interrupted!":
+           line_type = "spell_interrupted"
+    elif line_list[1] == "begins":  # assumes only spell messages have [player] begins...
+        if len(line_list) > 3:
+            if line_list[3] == "regenerate.":
+                line_list = "spell_regen"
+
+    # spell damage (has/was)
+    elif "has" in line_list:
+        if line_list[line_list.index("has") + 1] == "taken" and line_list[line_list.index("has") + 3] == "damage":
+            line_type = "dot_damage"
+    elif "was" in line_list:
+        if line_list[line_list.index("was") + 1] == "hit" and line_list[line_list.index("was") + 3] == "non-melee":
+            line_type = "spell_damage"
+
+    # engage messages
+    elif "engages" in line_list and line_list[-1].endswith("!"):
+        line_type = "engage"
+
+    # combat
+    #elif line_list[-2] == "but" and line_list[-1] == "misses!":
+    #    line_type = "melee_miss"
+    #elif line_list[-1] == "damage.":
+    #    if len(line_list) > 2:
+    #        if line_list[-3] == "points":
+    #            line_type = "melee_hit"
+
+    # other
+    # Damn falling messages causing a problem
+    #elif "injured" in line:
+    #    if line_list[2] == "injured":
+    #        if line_list[-1] == "falling.":
+    #            line_type == "fall_damage"
+
+    elif line_list[0] == "loading,":
+        line_type = "zoning"
+    elif line_list[0][0] == "*":
+        line_type = "random"
+    # chat from in game prompts or commands
+    elif line_list[1] == "Location" and line_list[0] == "Your":
+        line_type = "loc"
+    elif line_list[0] == "To" and line_list[1] == "join":
+        line_type = "group_invite"
+    elif line_list[0] == "there" and line_list[1] == "are":
+        line_type = "who_total"
+    elif line_list[0].startswith("["):
+        line_type = "who_player"
+    elif line_list[0] == "afk":
+        line_type = "who_player_afk"
+    elif line_list[0].startswith("<linedead>"):
+        line_type = "who_player_linkdead"
+    elif line_list[0] ==  "players" or line_list[0] == "Friends":
+        line_type = "who_top"
+    elif line_list[0] == "targeted" :
+        line_type = "target"
+
+    # possible emotes
+    elif line_list[1] == "bows" :
+        line_type = "emote_bow"
+    elif line_list[1] == "thanks" :
+        line_type = "emote_thank"
+    elif line_list[1] == "waves" :
+        line_type = "emote_wave"
+    elif line_list[1] == "dances" :
+        line_type = "emote_dance"
+    elif line_list[1] == "bonks" :
+        line_type = "emote_bonk"
+    elif line_list[1] == "beams" :
+        line_type = "emote_smile"
+    elif line_list[1] == "cheers." or line_list[1] == "cheers":
+        line_type = "emote_cheer"
+
+    # other
+    elif line_list[0] == "it":
+        if line_list[1] == "begins":
+            if "rain." in line:
+                line_type = "weather_start_rain"
+            if "snow." in line:
+                line_type = "weather_start_snow"
+        elif line_list[1] == "stops":
+            if "raining." in line:
+                line_type = "weather_stop_rain" # the sky clears as the rain ceases to fall.
+            if "snowing." in line:
+                line_type = "weather_stop_snow"
+        elif line_list[-1] == "camp.":
+            line_type == "you_camping"
+
+    return line_type
+
+if __name__ == '__main__':
+    main()
+
