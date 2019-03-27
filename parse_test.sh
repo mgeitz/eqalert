0ffaee9206ce8f3403a3dc2c52fca792ef6bccb5
mgeitz <mrgeitz@gmail.com>
2019-03-26 23:24:56 -0400

    Functions
    duh

--- parse_test.sh
+++ parse_test.sh
@@ -5,15 +5,53 @@
 #
 #
 #    * Ensure EQ Alert is running against the default character before testing
+#    * Presently needs to be run from the same directory as `config.json`
 #
 
 
-# Variables
+# Functions
+
+## Default Test: Append Message To Character Log
+function run_test() {
+  echo -e "\n  ${PURPLE}Type${NC} ${BLUE}:${NC} ${CYAN}$1${NC}"
+  echo -e "  ${LIGHT_GRAY}»${NC} ${LIGHT_GRAY}$2${NC}"
+  echo $2 >> "$CHAR_LOG"
+  sleep 0.018
+}
+
+## (Display) Skip Test
+function skip_test() {
+  echo -e "\n  ${PURPLE}Type${NC} ${BLUE}:${NC} ${CYAN}$1${NC}"
+  echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+}
+
+## Display Usage
+function test_usage() {
+  echo -e "\n${CYAN}Usage:${NC}"
+  echo -e "\n${WHITE}$ ${RED}Ensure EQ Alert is running${NC}"
+  echo -e "${WHITE}$ ${YELLOW}./parse_test.sh${NC}"
+}
+
+
+# Checks
+
+## Ensure EQ Alert is running
+ACTIVE=$(pgrep -f eqalert.py)
+if [ -z $ACTIVE ]; then
+  test_usage
+  exit 0
+fi
+
+
+# Settings
 
 ## Things worth changing
 EQ_LOGS=$(jq '.settings.paths.char_log' config.json)
 DEFAULT_CHAR=$(jq '.characters.default' config.json | tr -d '"')
 CHAR_LOG="${EQ_LOGS//\"}eqlog_${DEFAULT_CHAR^}_project1999.txt"
+
+
+# Constants
 
 ## Colors
 BLACK='\e[0;30m'
@@ -35,393 +73,357 @@ WHITE='\e[1;37m'
 NC='\e[0m'
 BOLD='\e[1m'
 
-## Runtime
-skip=0
-run=0
 
+# Ctrl+C Safety (Bash Trap)
 
-# Ctrl+C Safety
+## Stop everything
 function stop_tests() {
-  echo -e "\n${CYAN}Stopping tests${NC} ..."
+  echo -e "\n${CYAN}Stopping tests${NC} ${RED}.${WHITE}.${BLUE}.${NC}"
   exit 0
 }
 trap stop_tests SIGINT
 
 
-# Ensure EQ Alert is running
-ACTIVE=$(pgrep -f eqalert.py)
-if [ -z $ACTIVE ]; then
-  echo -e "\n${CYAN}Usage:${NC}"
-  echo -e "\n${WHITE}$ ${RED}Ensure EQ Alert is running${NC}"
-  echo -e "${WHITE}$ ${YELLOW}./parse_test.sh${NC}"
-  exit 0
-fi
+# Runtime Variables
+
+## Don't change these
+skip=0
+run=0
+
 
 
 # Begin the Trials
 
-## you_tell
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_tell${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+## You
+
+### you_tell
+msg="[Tue Jan 08 22:52:24 2019] You told Parser, 'Get off your merchant and raid!'"
+run_test "you_tell" "$msg"
+((run++))
+
+### you_say
+skip_test "you_say"
 ((skip++))
 
-## you_say
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_say${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_shout
+skip_test "you_shout"
 ((skip++))
 
-## you_shout
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_shout${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_group
+msg="[Tue Jan 08 22:58:40 2019] You tell your party, 'incoming'"
+run_test "you_group" "$msg"
+((run++))
+
+### you_ooc
+skip_test "you_ooc"
 ((skip++))
 
-## you_group
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_group${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_guild
+skip_test "you_guild"
 ((skip++))
 
-## you_ooc
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_ooc${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_auction
+skip_test "you_auction"
 ((skip++))
 
-## you_guild
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_guild${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_auction_wts:wtb
+skip_test "you_auction_wts:wtb"
 ((skip++))
 
-## you_auction
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_auction${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_auction_wts
+skip_test "you_auction_wts"
 ((skip++))
 
-## you_auction_wts:wtb
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_auction_wts:wtb${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_auction_wtb
+skip_test "you_auction_wtb"
 ((skip++))
 
-## you_auction_wts
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_auction_wts${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## you_auction_wtb
-iecho -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_auction_wtb${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## you_new_zone
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_new_zone${NC}"
+### you_new_zone
 msg="[Sat Dec 29 21:56:56 2018] You have entered The Wakening Lands."
-echo -e "  ${LIGHT_GRAY}»${NC} ${LIGHT_GRAY}msg${NC}"
-echo $msg >> "$CHAR_LOG"
-sleep 0.02
+run_test "you_new_zone" "$msg"
 msg="[Sat Dec 29 21:58:18 2018] You have entered Kael Drakkel."
-echo -e "  ${LIGHT_GRAY}»${NC} ${LIGHT_GRAY}msg${NC}"
-echo $msg >> "$CHAR_LOG"
-sleep 0.02
+run_test "you_new_zone" "$msg"
 msg="[Sun Dec 30 10:28:06 2018] You have entered Butcherblock Mountains."
-echo -e "  ${LIGHT_GRAY}»${NC} ${LIGHT_GRAY}msg${NC}"
-echo $msg >> "$CHAR_LOG"
-echo -e "  ${LIGHT_GRAY}»${NC} ${LIGHT_GRAY}run${NC} ${LIGHT_GREEN}✔${NC}"
+run_test "you_new_zone" "$msg"
 ((run++))
 
-## you_healed
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_healed${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_healed
+msg="[Sat Feb 02 15:39:26 2019] You have healed Parser for 470 points of damage."
+run_test "you_healed" "$msg"
+((run++))
+
+### you_afk_on
+skip_test "you_afk_on"
 ((skip++))
 
-## you_hungry
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_hungry${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_lfg_on
+msg="[Sun Jan 06 10:18:00 2019] You are now Looking For a Group."
+run_test "you_lfg_on" "$msg"
+((run++))
+
+### you_afk_off
+skip_test "you_afk_off"
 ((skip++))
 
-## you_afk_on
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_afk_on${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### you_lfg_off
+msg="[Thu Nov 01 10:07:49 2018] You are no longer Looking For a Group."
+run_test "you_lfg_off" "$msg"
+((run++))
+
+### you_outfood
+msg="[Tue Apr 19 20:54:46 2016] You are out of food."
+run_test "you_outfood" "$msg"
+((run++))
+
+### you_outdrink
+msg="[Tue May 17 17:23:10 2016] You are out of drink."
+run_test "you_outdrink" "$msg"
+((run++))
+
+### you_outfooddrink
+msg="[Thu Feb 08 20:59:13 2018] You are out of food and drink."
+run_test "you_outfooddrink" "$msg"
+((run++))
+
+### you_outfoodlowdrink
+msg="[Thu Mar 31 19:12:41 2016] You are out of food and low on drink."
+run_test "you_outfoodlowdrink" "$msg"
+((run++))
+
+### you_outdrinklowfood
+msg="[Thu Mar 31 19:12:41 2016] You are out of drink and low on food."
+run_test "you_outdrinklowfood" "$msg"
+((run++))
+
+### you_thirsty
+msg="[Thu Feb 08 19:00:19 2018] You are thirsty."
+run_test "you_thirsty" "$msg"
+((run++))
+
+### you_hungry
+msg="[Sat Apr 23 01:24:39 2016] You are hungry."
+run_test "you_hungry" "$msg"
+((run++))
+
+### you_spell_forget
+skip_test "you_spell_forgot"
 ((skip++))
 
-## you_lfg_on
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_lfg_on${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+
+## Spell
+
+### spell_something
+skip_test "spell_something"
 ((skip++))
 
-## you_afk_off
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_afk_off${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### spell_fizzle
+skip_test "spell_fizzle"
 ((skip++))
 
-## you_lfg_off
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_lfg_off${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### spell_interrupted
+skip_test "spell_interrupted"
 ((skip++))
 
-## you_outfood
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_outfood${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### spell_damage
+skip_test "spell_damage"
 ((skip++))
 
-## you_outdrink
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_outdrink${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### spell_break_charm
+skip_test "spell_break_charm"
 ((skip++))
 
-## you_outfooddrink
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_outfooddrink${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### spell_break_ensare
+skip_test "spell_break_ensare"
 ((skip++))
 
-## you_outfoodlowdrink
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_outfoodlowdrink${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### spell_break
+skip_test "spell_break"
 ((skip++))
 
-## you_outdrinklowfood
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_outdrinklowfood${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### dot_damage
+skip_test "dot_damage"
 ((skip++))
 
-## you_thirsty
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_thirsty${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+
+## Auction
+
+### auction
+skip_test "auction"
 ((skip++))
 
-## you_hungry
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_hungry${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### auction_wts
+skip_test "auction_wts"
 ((skip++))
 
-## you_spell_forget
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_spell_forgot${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### auction_wtb
+skip_test "auction_wtb"
 ((skip++))
 
-## spell_break_charm
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}spell_break_charm${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+
+## Emotes
+
+### emote_bow
+skip_test "emote_bow"
 ((skip++))
 
-## spell_break_ensare
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}spell_break_ensare${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### emote_thank
+skip_test "emote_thank"
 ((skip++))
 
-## spell_break
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}spell_break${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### emote_wave
+skip_test "emote_wave"
 ((skip++))
 
-## faction_line
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}faction_line${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### emote_dance
+skip_test "emote_dance"
 ((skip++))
 
-## target_cured
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}target_cured${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### emote_bonk
+skip_test "emote_bonk"
 ((skip++))
 
-## who_line
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}who_line${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### emote_smile
+skip_test "emote_smile"
 ((skip++))
 
-## mysterious_oner
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}mysterious_oner${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### emote_cheer
+skip_test "emote_cheer"
 ((skip++))
 
-## tell
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}tell${NC}"
+
+## Weather
+
+### weather_start_rain
+msg="[Sat Feb 10 20:31:41 2018] It begins to rain."
+run_test "weather_start_rain" "$msg"
+((run++))
+
+### weather_start_snow
+msg="[Sun Feb 11 14:42:56 2018] It begins to snow."
+run_test "weather_start_snow" "$msg"
+((run++))
+
+### weather_stop_rain
+skip_test "weather_stop_rain"
+((skip++))
+
+### weather_stop_snow
+skip_test "weather_stop_snow"
+((skip++))
+
+
+## Who
+
+### who_top
+msg="[Thu Feb 21 21:12:05 2019] Players in EverQuest:"
+run_test "who_top" "$msg"
+((run++))
+
+### who_line
+msg="[Sun Mar 03 09:05:21 2019] ---------------------------------"
+run_test "who_line" "$msg"
+((run++))
+
+### who_player
+msg="[Thu Feb 21 21:12:05 2019] [60 Hierophant] Indefinite (Wood Elf) <Tempest> ZONE: commons"
+run_test "who_player" "$msg"
+((run++))
+
+### who_player_afk
+msg="[Sun Nov 04 19:54:11 2018]  AFK [60 Hierophant] Phloem (Wood Elf) <Tempest>"
+run_test "who_player_afk" "$msg"
+((run++))
+
+### who_player_linkdead
+msg="[Sun Nov 04 19:48:03 2018]  <LINKDEAD>[60 Phantasmist] Dagner (Gnome) <Tempest>"
+run_test "who_player_linkdead" "$msg"
+((run++))
+
+### who_total
+msg="[Sun Mar 03 09:00:20 2019] There are no players in EverQuest that match those who filters."
+run_test "who_total" "$msg"
+msg="[Thu Feb 21 21:36:32 2019] There is 1 player in EverQuest."
+run_test "who_total" "$msg"
+msg="There are 4 players in EverQuest."
+run_test "who_total" "$msg"
+((run++))
+
+
+## Other
+
+skip_test "faction_line"
+((skip++))
+
+### target_cured
+skip_test "target_cured"
+((skip++))
+
+### you_camping
+msg="[Mon Nov 05 19:44:27 2018] It will take you about 30 seconds to prepare your camp."
+run_test "you_camping" "$msg"
+msg="[Fri Nov 09 18:17:05 2018] You abandon your preparations to camp."
+run_test "you_camping" "$msg"
+((run++))
+
+### tell
 msg="[Sat Dec 29 21:41:44 2018] Parser tells you, 'tell'"
-echo -e "  ${LIGHT_GRAY}»${NC} ${LIGHT_GRAY}msg${NC}"
-echo $msg >> "$CHAR_LOG"
-sleep 0.01
-echo -e "  ${LIGHT_GRAY}»${NC} ${LIGHT_GRAY}run${NC} ${LIGHT_GREEN}✔${NC}"
+run_test "tell" "$msg"
 ((run++))
 
-## guild
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}guild${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### guild
+msg="[Tue Jan 08 22:52:09 2019] Parser tells the guild, 'huzzah we are a guild'"
+run_test "guild" "$msg"
+((run++))
+
+### group
+msg="[Sat Dec 29 10:02:25 2018] Obbz tells the group, 'we have arrived'"
+run_test "group" "$msg"
+((run++))
+
+### say
+msg="Parser says, 'Incoming, here we go!'"
+run_test "say" "$msg"
+((run++))
+
+### shout
+msg="[Tue Nov 06 19:42:56 2018] Parser shouts, 'LF port to GD'"i
+run_test "shout" "$msg"
+((run++))
+
+### ooc
+msg="[Thu Feb 21 21:20:50 2019] Parser says out of character, 'Congratulations!'"
+run_test "ooc" "$msg"
+((run++))
+
+### engage
+msg="[Sat Dec 29 19:57:17 2018] a dracoliche engages Parser!"
+run_test "engage" "$msg"
+((run++))
+
+### zoning
+msg="[Sat Dec 29 21:55:59 2018] LOADING, PLEASE WAIT..."
+run_test "zoning" "$msg"
 ((skip++))
 
-## group
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}group${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### random
+skip_test "random"
 ((skip++))
 
-## say
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}say${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### loc
+skip_test "loc"
 ((skip++))
 
-## shout
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}shout${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### group_invite
+skip_test "group_invite"
 ((skip++))
 
-## ooc
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}ooc${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
+### target
+skip_test "target"
 ((skip++))
 
-## auction
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}auction${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## auction_wts
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}auction_wts${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## auction_wtb
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}auction_wtb${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## spell_something
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}spell_something${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## spell_fizzle
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}spell_fizzle${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## spell_interrupted
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}spell_interrupted${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## dot_damage
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}dot_damage${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## spell_damage
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}spell_damage${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## engage
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}engage${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## zoning
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}zoning${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## random
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}random${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## loc
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}loc${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## group_invite
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}group_invite${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## who_total
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}who_total${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## who_player
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}who_player${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## who_player_afk
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}who_player_afk${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## who_player_linkdead
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}who_player_linkdead${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## who_top
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}who_top${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## target
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}target${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## emote_bow
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}emote_bow${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## emote_thank
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}emote_thank${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## emote_wave
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}emote_wave${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## emote_dance
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}emote_dance${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## emote_bonk
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}emote_bonk${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## emote_smile
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}emote_smile${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## emote_cheer
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}emote_cheer${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## weather_start_rain
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}weather_start_rain${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## weather_start_snow
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}weather_start_snow${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## weather_stop_rain
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}weather_stop_rain${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## weather_stop_snow
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}weather_stop_snow${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
-
-## you_camping
-echo -e "${PURPLE}Type${NC} ${BLUE}:${NC} ${LIGHT_PURPLE}you_camping${NC}"
-echo -e "  ${LIGHT_GRAY}»${NC} ${YELLOW}skip${NC}"
-((skip++))
 
 
 # Tests completed
-echo -e " --- COMPLETED ---"
+echo -e "\n\n --- COMPLETED ---"
 echo -e "  ${LIGHT_GRAY}»${NC} ${CYAN}Skipped${NC} ${BLUE}:${NC} ${LIGHT_GRAY}$skip${NC}"
 echo -e "  ${LIGHT_GRAY}»${NC} ${CYAN}Run${NC} ${BLUE}:${NC} ${LIGHT_GRAY}$run${NC}"
