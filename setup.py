4707121bff34811c269c41a591a613f499ab40dc
Michael Geitz <michael@geitz.xyz>
2019-10-07 11:40:36 -0400

    Python Package (#25) :gift:
    * Python3 Package
    - Like a christmas present
    
    * Remove Need For Sub-Shell
    - no mas sub process

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='eqalert',
+    version='1.1.1',
+    author='Michael Geitz',
+    author_email='git@geitz.xyz',
+    install_requires=[
+        'pyinotify',
+        'playsound',
+        'gtts',
+    ],
+    python_requires='>3',
+    packages=['eqa', 'eqa.lib'],
+    license='LICENSE.txt',
+    entry_points={
+        "console_scripts": [
+            "eqa = eqalert.__main__:main",
+        ],
+    },
+    description='Parse and react to eqemu logs',
+    url='https://github.com/mgeitz/eqalert',
+)
