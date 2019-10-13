9c966014ea49f0f192883e01ed7865fb3ac07f9a
mgeitz <mrgeitz@gmail.com>
2019-10-12 20:19:31 -0400

    Merge Config Sections

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='eqalert',
-    version='1.4.0',
+    version='1.4.5',
     author='Michael Geitz',
     author_email='git@geitz.xyz',
     install_requires=[
