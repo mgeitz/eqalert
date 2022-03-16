504474988917a6a2b6909a14c3b35cf01bd27f8e
mgeitz <mrgeitz@gmail.com>
2022-03-16 19:20:52 -0400

    Missing `)`

--- eqa/lib/parser.py
+++ eqa/lib/parser.py
@@ -847,7 +847,7 @@ def check_who(line):
             return "who_player_anon"
         elif (
             re.fullmatch(
-                r"^\<LINKDEAD\>\[ANONYMOUS\] \w+ (?:( \<[a-zA-Z\s]+\>))$(?:( \<[a-zA-Z\s]+\> ZONE\: \w+| \<[a-zA-Z\s]+\>|)",
+                r"^\<LINKDEAD\>\[ANONYMOUS\] \w+ (?:( \<[a-zA-Z\s]+\>))$(?:( \<[a-zA-Z\s]+\> ZONE\: \w+| \<[a-zA-Z\s]+\>|))",
                 line,
             )
             is not None
