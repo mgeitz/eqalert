9000167b314fa14de0231b91acd9aaa21afa4602
mgeitz <mrgeitz@gmail.com>
2022-03-27 23:06:17 -0400

    Log All Line Matches
    - Adds a new debug level
    - Adds file size limits on debug files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="eqalert",
-    version="2.4.11",
+    version="2.5.3",
     author="Michael Geitz",
     author_email="git@geitz.xyz",
     install_requires=[
