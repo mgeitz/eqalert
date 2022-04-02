9e2abd649f490fdbcfeca1fabeecf726e64573c5
mgeitz <mrgeitz@gmail.com>
2022-04-02 13:40:05 -0400

    fixup! True or False Sound Matter Now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="eqalert",
-    version="2.7.4",
+    version="2.7.5",
     author="Michael Geitz",
     author_email="git@geitz.xyz",
     install_requires=[
