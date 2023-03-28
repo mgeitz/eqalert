eb207e13d27d8498ebe0285be3cb12d2185b0dc9
mgeitz <git@geitz.xyz>
2023-03-28 17:56:16 -0400

    Fix Location Location

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="eqalert",
-    version="3.4.6",
+    version="3.4.7",
     author="M Geitz",
     author_email="git@geitz.xyz",
     install_requires=[
