a04f17bd5cd530cab66f94de057bae61eeb1367b
M Geitz <git@geitz.xyz>
2023-06-12 06:26:39 -0400

    Fix Alert Consolidation (#210)
    - Was only playing spell timer alerts which would consolidation

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="eqalert",
-    version="3.5.4",
+    version="3.5.5",
     author="M Geitz",
     author_email="git@geitz.xyz",
     install_requires=[
